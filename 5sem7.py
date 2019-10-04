327d2901319ffd4c8b212ac1fd00b3b87c5b9ac0
AGusarov242 <55212600+AGusarov242@users.noreply.github.com>
2019-10-04 18:45:00 +0300

    Update 5sem7.py

--- 5sem7.py
+++ 5sem7.py
@@ -2,6 +2,8 @@ import numpy as np
 class Network:
 	def __init__(self, edges, v):
 		#self.weights_dict = []
+		global _infinity 
+		_infinity = 10 ** 9
 		a = []
 		b = []
 		for i in range(len(edges)):
@@ -10,27 +12,36 @@ class Network:
 		self.weights_dict = np.zeros((v+1,v+1))
 		for i in range(len(edges)):
 			self.weights_dict[a[i], b[i]] = int(edges[i][2])
-		global _infinity 
-		_infinity = 10 ** 9
-		self.min_path_to_i = [_infinity] * (v+1)
-		self.used = [False] * (v+1)
+		#print(self.weights_dict)
+		self.min_path_to_i = [_infinity] * (v)
+		self.used = [False] * (v)
 
 	def min_time(self, start, v):
 		self.min_path_to_i[start] = 0
 		ans = 0
-		v= 1
-		for i in range(len(self.used)):
+		p = 0
+		#print(self.min_path_to_i[p])
+		for i in range(v):
 			min_dist = _infinity
-			for j in range(len(self.used)):
+			for j in range(v):
+				#print(self.min_path_to_i[j])
 				if not self.used[j] and self.min_path_to_i[j] < min_dist:
 					min_dist = self.min_path_to_i[j]
+					#print(min_dist)
 					u = j
+					#print(u, 'aa')
 			ans += min_dist
+			#print(min_dist)
+			#print(ans)
 			self.used[u] = True
-			for v in range(len(self.used)):
-				self.min_path_to_i[v] = min(self.min_path_to_i[v], self.weights_dict[u][v])
-		print(self.min_path_to_i)
+			for p in range(v):
+				#print(self.min_path_to_i[p])
+				#print(self.weights_dict[u][p])
+				if self.weights_dict[u][p] != 0.0:
+					self.min_path_to_i[p] = min(self.min_path_to_i[p], self.weights_dict[u][p])
+				#print(self.min_path_to_i[p])
+		print(ans)
 				
 
-G = Network([[0, 3, 5], [1, 2, 11], [2, 3, 56], [4, 3, 77], [4, 5, 89]], 6)
+G = Network([[0, 3, 5], [2, 1, 11], [3, 2, 56], [1, 4, 77], [4, 5, 89]], 6)
 G.min_time(0, 6)
