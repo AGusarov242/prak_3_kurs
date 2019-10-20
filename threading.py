fe30f8908185a56bd70223c28da97fcedddcffcc
AGusarov242 <55212600+AGusarov242@users.noreply.github.com>
2019-10-20 23:39:38 +0300

    Update threading.py

--- threading.py
+++ threading.py
@@ -6,11 +6,10 @@ import numpy as np
 import soundfile
 import audioread
 
-from multiprocessing import Process
-from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
+from concurrent.futures import ProcessPoolExecutor, as_completed
 
-def extract_mfcc(sub_folder, output_folder):
-    output_path = "{}/{}".format(output_folder, sub_folder[0])
+def extract_mfcc(sub_folder, output_folder, path):
+    output_path = "{}/{}".format(output_folder, path)
     os.makedirs(output_path, exist_ok=True)
     for i in sub_folder[2]:
         audio_time_series, sampling_rate = librosa.core.load("{}/{}".format(sub_folder[0],i))
@@ -19,9 +18,12 @@ def extract_mfcc(sub_folder, output_folder):
         np.save("{}/{}".format(output_path, i), mfcc_features)
 
 
-def threads(input_folder, output_folder, threads_count = 7):
+def threads(input_folder, output_folder, threads_count):
     main_folder = os.walk(input_folder)
     with ThreadPoolExecutor(max_workers=threads_count) as pool:
         for sub_folder in main_folder:
+            path = sub_folder[0]
             if sub_folder[2]:
-                pool.submit(extract_mfcc, sub_folder, output_folder)
+                pool.submit(extract_mfcc, sub_folder, output_folder, path)
+                
+threads("aac1", "new_aac", 7)
